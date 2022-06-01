923ff3b2f7b61bee61ce2fdad053247d62ab11da
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-06-01 20:57:19 -0300

    BASIC DESIGN FORM LOGIN

--- frontend/src/pages/LoginScreen.js
+++ frontend/src/pages/LoginScreen.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const LoginScreen = () =>{
-    return <h1>Not Register</h1> 
+    return (
+        <section className="container">
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </section>
+    )
 }
 
 export default LoginScreen;
