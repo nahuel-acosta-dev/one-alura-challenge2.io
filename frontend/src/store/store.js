07abb7739bfaafc41f9de405d686e759a63a2b10
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-06-01 21:49:41 -0300

    Add extension redux Settings

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -5,4 +5,8 @@ const reducers = combineReducers({
     auth: authReducers,
 })
 
-export const store = createStore(reducers);
+export const store = createStore
+    (
+        reducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
