29aa6e5d6f2f49adc04214e8d7c38aeda1d699a7
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-06-01 21:36:50 -0300

    Provider added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import HelperRouters from './routes/HelperRoutes';
 import Header from './components/header/Header';
+import {Provider} from "react-redux";
 
 const App = () => {
     //const show = false;
     return(
-        <>
+        <Provider>
             <Header />
             <main>
                 <Container className="main__container" fluid>
                     {
                     <HelperRouters />
-                    /*show ?
-                        //<Home />
-                        
-                        <SaveScreen />
-                        :
-                        <GameStarts />*/
                     }
                 </Container>
             </main>
-        </>
+        </Provider>
     )
 }
 //Implememntar esto en los componentes, es el tipo de datos que se recibira del snipet
