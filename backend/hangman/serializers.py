72eda20256bba69890b42a3d5abc6a41b871c86e
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-06-18 22:09:05 -0300

    Login serializers now returns ID too

--- backend/hangman/serializers.py
+++ backend/hangman/serializers.py
@@ -19,7 +19,7 @@ class LogoutUserSerializer(serializers.ModelSerializer):
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
 
 class UserSerializer(serializers.ModelSerializer):
