7a23c96bdfc69d54eaaa16ea841d26332df8a863
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-07-20 15:16:11 -0300

    created basic tests

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,11 +1,24 @@
 import pytest
-from hangman.models import User  
+from hangman.models import User
 
+
+@pytest.mark.django_db
 def test_user_creation():
     user = User.objects.create(
-       username='cris',
-       email='cris@example.com',
-       password='cris00'
+        username='cris',
+        email='cris@example.com',
+        password='cris00'
     )
-    
+
     assert user.username == 'cris'
+
+
+@pytest.mark.django_db
+def test_superuser_creation():
+    user = User.objects.create_superuser(
+        username='chris',
+        email='chris@example.com',
+        password='chris00'
+    )
+
+    assert user.is_superuser
