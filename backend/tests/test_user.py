98424dbe7c535610e6e38208206b5c8ec46e06f4
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-07-19 21:06:21 -0300

    update requirements

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,1 +1,11 @@
 import pytest
+from hangman.models import User  
+
+def test_user_creation():
+    user = User.objects.create(
+       username='cris',
+       email='cris@example.com',
+       password='cris00'
+    )
+    
+    assert user.username == 'cris'
